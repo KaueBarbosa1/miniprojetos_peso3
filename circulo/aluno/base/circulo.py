0b2440669f1fbf5a22de988b520ff33b731a0a49
Kauê Barbosa <kauebarbosa1k@gmail.com>
2023-11-17 23:45:55 -0300

    arq_novos

---
+++ circulo/aluno/base/circulo.py
@@ -0,0 +1,19 @@
+from circulo.cliente.circulo_base import CirculoBase
+
+
+
+class Circulo(CirculoBase):
+
+    def __init__(self, id: str, limite: int):
+        super().__init__(id, limite)
+        self.contatos = []
+
+    def getNumberOfContacts(self):
+        return len(self.contatos)
+
+    def setLimite(self, limite: int):
+        self.limite = limite
+        return self.limite
+
+    def __eq__(self, other):
+        return self.id == other.id and self.limite == other.limite
---
+++ gestao-universitaria/src/terceirizado.py
@@ -0,0 +1,8 @@
+from funcionario import Funcionario
+
+class Terceirizado(Funcionario):
+
+    def __init__(self, cpf: str, nome: str, insalubre: bool):
+        self.cpf = cpf
+        self.nome = nome
+        self.insalubre = insalubre
